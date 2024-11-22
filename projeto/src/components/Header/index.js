91d560462a934e74b5caf83251e61000a8679bd1
joaobent <joaopedro0744@hotmail.com>
2024-11-22 07:51:48 -0400

    a

--- projeto/src/components/Header/index.js
+++ projeto/src/components/Header/index.js
@@ -1,28 +1,21 @@
-import Link from "next/link"
-import styles from './header.module.css'
+'use client';
+import Link from "next/link";
+import {useState} from "react"
 
-export default function Header() {
-    return(
-        <header className={styles.header}>
-            <nav>
-                <ul className={styles.lista}>
-                    <li>
-                        <Link href={'/sobre'}>
-                        sobre
-                        </Link>
-                        
-                    </li>
-                    <li>
-                    <Link href={''}>
-                        contato
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href={'/states'}>state</Link>
-                    </li>
-                </ul>
-            </nav>
+export default function Header (){
+    const [showMenu, setShowMenu] = useState(true)
+    return (
+        <header>
+            <button onClick={()=>setShowMenu(!showMenu)}>Clique</button>
+            {
+                showMenu &&
+                <nav>
+                    <ul>
+                        <li><Link href="/sobre">Sobre</Link></li>
+                        <li><Link href="/contato">Contato</Link></li>
+                    </ul>
+                </nav>
+            }
         </header>
     )
-    
 }
